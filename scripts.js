42af7edf02c5388b5e52d9001334481725c6a0bb
sean-nl <sean.n.lamb.arch@gmail.com>
2024-04-16 19:57:21 -0400

    Update stying
    Update styling to be similar to a Texas Instruments
    TI-30X.
    For aesthetic reasons, minor changes to HTML and js:
    function buttons moved to top row.

--- scripts.js
+++ scripts.js
@@ -1,66 +1,91 @@
 let a = 0
 let b = 0;
 let operator;
-let display = 0;
+let currentVal = 0;
+let resetInput = true; // if true, the next number input will replace the current number being displayed.
+const DECIMAL_PLACES = 6;
 
-let displayText = document.querySelector('#display');
+let display = document.querySelector('#display');
 init();
 
-//Todo: make the names and responsibilities of "display" vs "displayText" consistent.
-//This should be legible in the code and redundant assignments should be removed.
+//Todo 2.0: display error when dividing by 0. DONE.
 
-//Todo 2: clean up code (don't take too long on this... as per odin proj recommendations)
+//Todo 3: the operator reset issue
+//once "equal" has been hit, pressing more buttons does not reset the display, but adds more vals.
+//this to be fixed.
+
+//Todo 3: clean up code (don't take too long on this... as per odin proj recommendations)
+//probbably can use resetinput intelligently and simplify the number button event listeners.
 
 function init() {
-    displayText.textContent = display;
+    display.textContent = 0;
+    
+    //For displaying numbers
     const numberButtons = document.querySelectorAll('.num');
-
-    //listener for displaying numbers
     for (const num of numberButtons) {
-        num.addEventListener('click', () => {
-            display = ( display === 0 ) ? num.textContent : String(display).concat(num.textContent);
-            displayText.textContent = display;
-        });
+
+        //If user presses 0 and 0 is displayed, don't do anything.
+        //Otherwise, update the currentval and display it.
+        if (num.textContent === '0') {
+            num.addEventListener('click', () => {
+                if (Number(display.textContent) !== 0) {
+                    currentVal = ( currentVal === 0 ) ? num.textContent : Number(String(currentVal).concat(num.textContent));
+                    resetInput = false;
+                    display.textContent = roundToDecimal(currentVal, DECIMAL_PLACES);
+                }
+            });
+        }
+        else {
+            num.addEventListener('click', () => {
+                currentVal = ( currentVal === 0 || resetInput === true) ? num.textContent : Number(String(currentVal).concat(num.textContent));
+                resetInput = false;
+                display.textContent = roundToDecimal(currentVal, DECIMAL_PLACES);
+            });
+        }
+
     }
 
-    //listener for adding numbers
+    //Operators
     const operatorBtns = document.querySelectorAll('.operator');
     for (const op of operatorBtns) {
         op.addEventListener('click', () => {
-
             if (operator) {
-                display = operate(a, displayText.textContent, operator);
-                displayText.textContent = display;                   
+                currentVal = operate(a, display.textContent, operator);
+                isNumeric(currentVal) ? display.textContent = roundToDecimal(currentVal, DECIMAL_PLACES) : display.textContent = currentVal;            
             }
-
             operator = op.textContent;
-            a = display;
-            display = 0;
-            // displayText.textContent = display;
-
+            a = currentVal;
+            currentVal = 0;
         });
     }
 
-    //equals button
+    //Equals button
     const eqBtn = document.querySelector('#evaluate');
     eqBtn.addEventListener('click', () => {
             if (operator) {
-                display = operate(a, displayText.textContent, operator);
-                displayText.textContent = display;
+                currentVal = operate(a, display.textContent, operator);
+                isNumeric(currentVal) ? display.textContent = roundToDecimal(currentVal, DECIMAL_PLACES) : display.textContent = currentVal;
                 operator = '';
+                resetInput = true;
             }
     });
 
-    //clr button
+    
+
+    //Clr button
     const clrBtn = document.querySelector('#clr');
     clrBtn.addEventListener('click', () => {
-        display = 0;
-        displayText.textContent = display;
+        currentVal = 0;
+        display.textContent = roundToDecimal(currentVal, DECIMAL_PLACES);
         operator = '';
         a = 0;
         b = 0;
     });
 
+}
+
+function roundToDecimal(x,d) {
+    return Math.round(x * Math.pow(10,d)) / Math.pow(10,d);
 }
 
 function add(a, b) {
@@ -76,7 +101,12 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return Number(a) / Number(b);
+    if (Number(b) === 0) {
+        return "ERROR: DIVISION BY ZERO";
+    }
+    else {
+        return Number(a) / Number(b);
+    }
 }
 
 function operate(a, b, operator) {
@@ -85,9 +115,14 @@ function operate(a, b, operator) {
             return add(a,b);
         case "-":
             return subtract(a,b);
-        case "*":
+        case "x":
             return multiply(a,b);
         case "/":
             return divide(a,b);
     }
+}
+
+//reference: https://stackoverflow.com/questions/175739/how-can-i-check-if-a-string-is-a-valid-number
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
 }
